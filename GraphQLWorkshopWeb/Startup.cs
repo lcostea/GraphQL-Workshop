a5f00bd52a34852f73e7540ce5b7e09cfa5dfcb2
Liviu Costea <email.lcostea@gmail.com>
2018-05-30 21:56:44 +0300

    configuration

--- GraphQLWorkshopWeb/Startup.cs
+++ GraphQLWorkshopWeb/Startup.cs
@@ -1,20 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace GraphQLWorkshopWeb
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; private set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddJsonFile("settings.json");
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +28,6 @@ namespace GraphQLWorkshopWeb
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
         }
     }
 }
